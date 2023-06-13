f51a7d2dd646eda6e107df827db50306fde72acb
Isira Adithya <rp.isira@gmail.com>
2023-06-13 11:56:06 +0530

    Added files of 13/06/2023

---
+++ 07062023/main.js
@@ -0,0 +1,12 @@
+document.write("External Javascript is fun.<br>")
+// confirm("Are you sure?")
+// prompt("Test", "Dummy")
+
+no1 = parseInt(prompt("Enter the number 1"));
+no2 = parseInt(prompt("Enter the number 2"));
+
+document.write("Addition: " + (no1 + no2) + "<br>");
+document.write("Substraction: " + (no1 - no2) + "<br>");
+document.write("Multiplication: " + (no1 * no2) + "<br>");
+document.write("Division: " + (no1 / no2) + "<br>");
+
